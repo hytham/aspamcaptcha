cfd6a002bbb3ac62fdec32fef80db732697a40f2
hytham Alobydi <hythamalobydi@hythams-MacBook-Pro.local>
2016-11-01 09:01:59 -0400

    090111012016

---
+++ cognetik_captcha.ts
@@ -0,0 +1,13 @@
+// This type script 
+class cognetik_captcha{
+    x:number; // The left side of the equestion
+    y:number; // The right side of the equestion
+    r:number; // The result of the equestion
+    // This constructor will take the number of selections to be displayed
+    // total_selections : The total options to select from
+    // wrongccsclass : The wrong css class 
+    // wrongccsclass : The right css class
+    constructor(total_selections:number,wrongccsclass:string,rightcssclass:string){
+
+    }
+}
