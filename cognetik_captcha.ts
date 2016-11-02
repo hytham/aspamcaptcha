27ddebf6577c56eda2102d6d9a766b7944327b97
hytham Alobydi <hythamalobydi@hythams-MacBook-Pro.local>
2016-11-02 11:27:20 -0400

    112711022016

--- cognetik_captcha.ts
+++ cognetik_captcha.ts
@@ -17,19 +17,14 @@ class randomeNumbers{
     }
     // rteurn the html reprentation of the object
     toHtml():string{
-
         return "<div class='row'><div class='md-col-6'></div>"+this.x +"+"+this.y+"<div class='md-col-6'><span class='"+this.css+"'>"+this.ans+"</span></div></div>";
     }
 
 }
 class cognetik_captcha{
 
-    
-    x:number; // The left side of the equestion
-    y:number; // The right side of the equestion
-    r:number; // The result of the equestion
-    wcss:string;// The wrong css class
-    rcss:string;// The right css class
+    html:string;// the html snippet
+    divtag:string; // The dive tag
     randomeNumbers:randomeNumbers[]; // Array of all the wrndome objects
     
     // This constructor will take the number of selections to be displayed
@@ -37,25 +32,38 @@ class cognetik_captcha{
     // wrongccsclass : The wrong css class 
     // wrongccsclass : The right css class
     // dividtarg: The div target that will host the controllers
+    // The minmum range
+    // The maximum range
     constructor(
-        total_selections:number,
-        wrongccsclass:string,
-        rightcssclass:string,
-        dividtarg:string, // The aprent div tag to ebbed 
-        min:number, // The min number to generate for the randome number
-        max:number // The max number to generate for the randome number
+        total_selections:number = 3,
+        wrongccsclass:string = "wronganswer",
+        rightcssclass:string = "rightanswer",
+        dividtarg:string="cognetik-captcha", // The aprent div tag to ebbed 
+        min:number= 0, // The min number to generate for the randome number
+        max:number =3// The max number to generate for the randome number
         ){
         // TODO Add code to sore the dfault seetings and initalise the engine
+
+        this.divtag=dividtarg; // Assign the 
+
         let css="wrong-message";
         let ans="you are a robot";
+
         this.randomeNumbers=new randomeNumbers[total_selections];
         for(var i=1;i<total_selections;i++)
              this.randomeNumbers[i]=new randomeNumbers(min,max,css,ans); 
         
         
-    }
+    };
+    
+    
+
     // This function will Render the full 
     Render():void{
-
+        for(var i=1;i<this.randomeNumbers.length;i++)
+        {
+            this.html+=this.randomeNumbers[i].toHtml();
+        } 
+        document.getElementById(this.divtag).innerHTML=this.html;
     }
 }
